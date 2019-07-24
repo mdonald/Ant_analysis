6a9d27745a4b769a6a2a7a754d3a586189742a35
Marion Donald <mld675@gmail.com>
2019-07-24 03:49:38 -0500

    chao richness esimtate, and diversity esimates for shannon and simpson. Ran linear models on proportion of ant abundance and ant species -- linear models do not fit the data well for the ant abundance vs. alpha diversity (based on the residuals plots) but do a better job with the ant spp occurrence data vs alpha diversity

--- Ant_analysis_RScript.R
+++ Ant_analysis_RScript.R
@@ -11,11 +11,17 @@ library(dplyr)
 library(tidyr)
 library(vegan)
 library(ggplot2)
+library(BiodiversityR)
+library(extrafont)
+library(ggpubr)
+library(broom)
 
 ## read in data
 data_MH <- read_excel("Meghan Hager Big Thicket Ant Data Updated 07-22-19 (1).xlsx")
 
 data_GZ_all <- read_excel("big_thicket_ants20142018.xlsx", sheet = "Pitfall Data")
+
+native_classification <- read.csv("ant_species_native_classification.csv") 
 
 
 
@@ -98,7 +104,7 @@ data_GZ_wide <- data_GZ_clean %>%
   #  mutate(grouped_id = row_number()) %>% 
   spread(key = "species", value = "Abundance") %>% 
   mutate_all(~replace(., is.na(.), 0)) %>%  ## make all the NAs under the species names zeros (for this analysis) 
-  select(-Year:-Month) ## drop the identifiers and just keep the species names
+  select(-Site:-Month) ## drop the identifiers and just keep the species names and Year
 
 
 ## find the species that are in MH that we need to add to this df in order to merge them
@@ -129,10 +135,17 @@ data_MH_2a <- data_MH_clean %>%
          "Solenopsis nickersoni" = 0, 
          "Tapinoma sessile" = 0) %>% 
   select("Aphaenogaster carolinensis":"Tapinoma sessile",
-         -Site_Code, -`Inside or Outside (Inside=1)`)
+         -Site_Code, -`Inside or Outside (Inside=1)`) %>% 
+  mutate(Year = 2015)
 
-data_spacc <- data_MH_2a %>% 
+data_spacc_all <- data_MH_2a %>% 
   bind_rows(data_GZ_wide2)
+
+data_spacc_year <- data_spacc_all %>% 
+                  select(Year)
+
+data_spacc <- data_spacc_all %>% 
+              select(-Year) # drop year, just keep the species by site matrix
 
 curve <- specaccum(data_spacc, method = "exact")
 
@@ -152,6 +165,205 @@ ggplot(spacc_df, aes(sites, richness))+
   theme_classic()+
   labs(x = "Number of pitfall traps",
        y = "Species richness")
+
+## Chao estimated (37 species observed, 43 +/- 6 species predicted by Chao, 43 predicted back jack1 +/- 2)
+richness_est_df <- vegan::specpool(data_spacc)
+
+
+data_spacc_df <- as.data.frame(data_spacc)
+
+
+## calculate species rank abundance using the rankabundance package from BiodiversityR (only accepts dataframes not tibbles)
+RankAbun.1 <- BiodiversityR::rankabundance(as.data.frame(data_spacc))
+
+## conver to df for ggplot
+rank_abu_df <- as.data.frame(RankAbun.1) %>% 
+  rownames_to_column(var = "species") #%>% 
+
+## native classification df to merge
+native_info <- native_classification %>% 
+  rename(species = "Species") %>% 
+  mutate(species = as.character(species))
+
+
+rank_abu_df2 <- rank_abu_df  %>% 
+  full_join(native_info) %>% 
+  mutate(Native = as.factor(Native)) 
+
+## Rank abundance curve 
+ggplot(rank_abu_df2, aes(x=rank, y = abundance, label=species))+
+  geom_point(aes(color = Native, shape = Native))+
+  geom_line()+
+  theme_classic()+
+  labs(x = "Species rank",
+       y = "Abundance")+
+  geom_text(aes(label=ifelse(abundance>150,as.character(species),'')),hjust=-0.05,vjust=0, check_overlap = T)+
+  scale_color_manual(values = c("#055864", "#04C0DD", "gray49"))+
+  scale_shape_manual(values = c(16,17,21))#+
+  #theme(text = element_text(family = "Times New Roman"))
+
+## diversity measures -- no difference in alpha diversity across the years
+shannon_div <- diversity(data_spacc, "shannon")
+
+shannon_div_df <- as.data.frame(shannon_div) %>% 
+                  bind_cols(data_spacc_year) %>% 
+  mutate(Year = as.factor(Year))
+
+ggplot(shannon_div_df, aes(x = Year, y = shannon_div))+
+  geom_boxplot()+
+  geom_jitter(width = 0.05, height = 0)+
+  theme_classic()+
+  labs(x = "Sampling year",
+       y = "Shannon diversity index")+
+  stat_compare_means(method = "t.test")
+
+## simpson div - no difference in alpha diversity across the years
+simpson_div <- diversity(data_spacc, "simpson")
+
+simpson_div_df <- as.data.frame(simpson_div) %>% 
+  bind_cols(data_spacc_year) %>% 
+  mutate(Year = as.factor(Year))
+
+ggplot(simpson_div_df, aes(x = Year, y = simpson_div))+
+  geom_boxplot()+
+  geom_jitter(width = 0.05, height = 0)+
+  theme_classic()+
+  labs(x = "Sampling year",
+       y = "Simpson diversity index")+
+  stat_compare_means(method = "t.test")
+
+## get df with info on sample ID (from row number) and year to get proportion of non-native to native ants (and also do it as occurrence for sp)
+data_spacc_all_tidy <- data_spacc_all %>% 
+  rownames_to_column("ID") %>% 
+  gather(key = "species", value = "abundance", c(-Year, -ID)) %>% 
+  full_join(native_info)
+  
+data_spacc_all_tidy_abu <- data_spacc_all_tidy %>% 
+  group_by(ID, Year, Native) %>% 
+  summarize(abundance = sum(abundance))
+
+total_abu <- data_spacc_all_tidy_abu %>% 
+  ungroup() %>% 
+  group_by(ID, Year) %>% 
+  summarize(total_abundance = sum(abundance))
+
+non_native_abu <- data_spacc_all_tidy_abu %>% 
+  filter(Native == 0) %>% 
+  rename(non_native_abu = abundance) %>% 
+  select(-Native)
+
+non_native_propotion_df <- total_abu %>% 
+  left_join(non_native_abu) %>% 
+  mutate(non_native_abu_prop = non_native_abu/total_abundance) %>% 
+  ungroup() %>% 
+  mutate(ID = as.numeric(ID)) %>% 
+  arrange(ID) %>% 
+  bind_cols(shannon_div_df,
+            simpson_div_df)
+
+#  linear model
+lm_fit <- lm(shannon_div ~ non_native_abu_prop, data=non_native_propotion_df)
+summary(lm_fit)
+plot(lm_fit) ## residuals are not "random" -- they form an inverted U, linear model is not a great fit for these data
+tidy(lm_fit)
+
+glance(lm_fit)
+
+## looks like there's a hump-shaped relationship between alpha diversity and proportion of non-native ants,
+## not sure that the linear model is the best fit for these data
+ggplot(non_native_propotion_df, aes(non_native_abu_prop, shannon_div))+
+  geom_point()+
+  geom_line(data = broom::augment(lm_fit), aes(x = non_native_abu_prop, y = .fitted))+
+  #facet_grid(~Year)+
+  theme_classic()+
+    labs(x = "Proportion of non-native ant abundance",
+         y = "Shannon diversity index")
+
+### for simpson div
+#  linear model
+lm_fit_simp<- lm(simpson_div ~ non_native_abu_prop, data=non_native_propotion_df)
+summary(lm_fit_simp)
+
+plot(lm_fit_simp)
+tidy(lm_fit_simp)
+
+glance(lm_fit_simp)
+
+## looks like there's a hump-shaped relationship between alpha diversity and proportion of non-native ants,
+## not sure that the linear model is the best fit for these data
+ggplot(non_native_propotion_df, aes(non_native_abu_prop, simpson_div))+
+  geom_point()+
+  geom_line(data = broom::augment(lm_fit), aes(x = non_native_abu_prop, y = .fitted))+
+  #facet_grid(~Year)+
+  theme_classic()+
+  labs(x = "Proportion of non-native ant abundance",
+       y = "Shannon diversity index")
+
+### do the same as above but this time by species occurrence (presence/absence) rather than abundance
+data_spacc_all_tidy_occ <- data_spacc_all_tidy %>% 
+  filter(abundance != 0) %>%  # sp has to be present to contribute
+ filter(Native == 1) %>% 
+  group_by(ID, Year) %>% 
+  summarize(num_native_sp = n())
+
+data_spacc_all_tidy_non_native <- data_spacc_all_tidy %>% 
+  filter(abundance != 0) %>%  # sp has to be present to contribute
+  filter(Native == 0 | Native == 2) %>% 
+  group_by(ID, Year) %>% 
+  summarize(num_nonnative_sp = n())
+
+occurrence_proportion_df <- data_spacc_all_tidy_occ %>% 
+  full_join(data_spacc_all_tidy_non_native) %>% 
+  mutate_all(~replace(., is.na(.), 0)) %>% 
+  mutate(total_sp = num_native_sp + num_nonnative_sp,
+         prop_nonnative_sp = num_nonnative_sp/total_sp) %>% 
+  ungroup() %>% 
+  mutate(ID = as.numeric(ID)) %>% 
+  arrange(ID) %>% 
+  bind_cols(shannon_div_df, 
+            simpson_div_df)
+
+
+### for shannon div
+#  linear model
+lm_fit_shan_occ<- lm(shannon_div ~ prop_nonnative_sp, data=occurrence_proportion_df)
+summary(lm_fit_shan_occ)
+
+plot(lm_fit_shan_occ)
+tidy(lm_fit_shan_occ)
+
+glance(lm_fit_shan_occ)
+
+## residuals plot looks more "random" -- linear model is more appropriate for these data than the abundance proportion
+ggplot(occurrence_proportion_df, aes(prop_nonnative_sp, shannon_div))+
+  geom_point()+
+  geom_line(data = broom::augment(lm_fit_shan_occ), aes(x = prop_nonnative_sp, y = .fitted))+
+  #facet_grid(~Year)+
+  theme_classic()+
+  labs(x = "Proportion of non-native ant species",
+       y = "Shannon diversity index")
+
+### for simpson div
+#  linear model
+lm_fit_simp_occ<- lm(simpson_div ~ prop_nonnative_sp, data=occurrence_proportion_df)
+summary(lm_fit_simp_occ)
+
+plot(lm_fit_simp_occ)
+tidy(lm_fit_simp_occ)
+
+glance(lm_fit_simp_occ)
+
+## residuals plot looks more "random" -- linear model is more appropriate for these data than the abundance proportion
+ggplot(occurrence_proportion_df, aes(prop_nonnative_sp, simpson_div))+
+  geom_point()+
+  geom_line(data = broom::augment(lm_fit_simp_occ), aes(x = prop_nonnative_sp, y = .fitted))+
+  #facet_grid(~Year)+
+  theme_classic()+
+  labs(x = "Proportion of non-native ant species",
+       y = "Simpson diversity index")
+
+
+
 
 
 
