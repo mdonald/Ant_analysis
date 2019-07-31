5d547516219eb8e5d27b3ee7be76d02efa813466
Marion Donald <mld675@gmail.com>
2019-07-31 16:58:24 -0500

    Going with PCoA analysis with pitfalls pooled at the site level

--- Ant_analysis_RScript.R
+++ Ant_analysis_RScript.R
@@ -17,6 +17,9 @@ library(ggpubr)
 library(broom)
 library(gridExtra)
 library(cowplot)
+library(ggpubr)
+library(wesanderson)
+
 
 ## read in data
 data_MH <- read_excel("Meghan Hager Big Thicket Ant Data Updated 07-22-19 (1).xlsx")
@@ -302,6 +305,39 @@ non_native_propotion_df <- total_abu %>%
   bind_cols(shannon_div_df,
             simpson_div_df)
 
+non_native_propotion_df_cat <- non_native_propotion_df %>% 
+  filter(total_abundance != 0) %>% 
+  mutate(category = ifelse(non_native_abu_prop < .33, "Low", 
+                           ifelse(non_native_abu_prop > .66, "High", "Medium"))) %>% 
+  left_join(sp_richness, by = "ID")
+
+non_native_propotion_df_cat$category <- factor(non_native_propotion_df_cat$category, levels = c("Low", "Medium", "High"))
+
+my_comparisons <- list(   c("High", "Medium"),  c("Low", "Medium"), c("Low", "High") )
+
+
+
+non_native_propotion_df_cat_tidy <- non_native_propotion_df_cat %>% 
+  select(ID, Year, non_native_abu_prop, shannon_div, simpson_div, total_sp, category) %>% 
+  gather(diversity_type, diversity_measure, -c(ID, Year, non_native_abu_prop, category)) %>% 
+  mutate(diversity_type = case_when(diversity_type == "shannon_div" ~ "Shannon diversity index",
+                                    diversity_type == "simpson_div" ~ "Simpson diversity index",
+                                    diversity_type == "total_sp" ~ "Species richness"))
+
+abundance_prop_alpha_fig <- ggplot(non_native_propotion_df_cat_tidy, aes(category, diversity_measure, fill = category))+
+  geom_boxplot(position =position_dodge())+
+  geom_point(position = position_jitterdodge(0.25), alpha = .75)+
+  stat_compare_means(comparisons = my_comparisons, aes(label = ..p.signif..))+
+  scale_fill_manual(values = wes_palette("GrandBudapest1"))+
+  scale_color_manual(values = wes_palette("GrandBudapest1"))+
+  labs(y = "Alpha diversity",
+       x = "Proportion of non-native species (by abundance P.flavens as native)")+
+  theme_classic()+
+  theme(text = element_text(family = "Times New Roman", size = 14),
+        legend.position = "bottom",
+        legend.title = element_blank())+
+  facet_grid(diversity_type~., scales="free_y")
+
 #  linear model
 lm_fit <- lm(shannon_div ~ non_native_abu_prop, data=non_native_propotion_df)
 summary(lm_fit)
@@ -349,7 +385,7 @@ data_spacc_all_tidy_occ <- data_spacc_all_tidy_PF_native %>%
 
 data_spacc_all_tidy_non_native <- data_spacc_all_tidy %>% 
   filter(abundance != 0) %>%  # sp has to be present to contribute
-  filter(Native == 0 | Native == 2) %>% 
+  filter(Native == 0) %>% 
   group_by(ID, Year) %>% 
   summarize(num_nonnative_sp = n())
 
@@ -377,7 +413,7 @@ glance(lm_fit_shan_occ)
 
 ## residuals plot looks more "random" -- linear model is more appropriate for these occurrence data than the abundance proportion
 ggplot(occurrence_proportion_df, aes(prop_nonnative_sp, shannon_div))+
-  geom_point()+
+  geom_jitter()+
   geom_line(data = broom::augment(lm_fit_shan_occ), aes(x = prop_nonnative_sp, y = .fitted))+
   #facet_grid(~Year)+
   theme_classic()+
@@ -397,12 +433,47 @@ glance(lm_fit_simp_occ)
 
 ## residuals plot looks more "random" -- linear model is more appropriate for these data than the abundance proportion
 ggplot(occurrence_proportion_df, aes(prop_nonnative_sp, simpson_div))+
-  geom_point()+
+  geom_jitter()+
+  geom_smooth()+
   geom_line(data = broom::augment(lm_fit_simp_occ), aes(x = prop_nonnative_sp, y = .fitted))+
   #facet_grid(~Year)+
   theme_classic()+
   labs(x = "Proportion of non-native ant species",
        y = "Simpson diversity index")
+
+occurrence_proportion_df_cat <- occurrence_proportion_df %>% 
+  filter(total_sp != 0) %>% 
+  mutate(category = ifelse(prop_nonnative_sp < .33, "Low", 
+                           ifelse(prop_nonnative_sp > .66, "High", "Medium")))
+
+occurrence_proportion_df_cat$category <- factor(occurrence_proportion_df_cat$category, levels = c("Low", "Medium", "High"))
+
+my_comparisons <- list(   c("High", "Medium"),  c("Low", "Medium"), c("Low", "High") )
+
+## get species richness (total # sp per trap) to use as the species richness diversity measure by abundance later
+sp_richness<- occurrence_proportion_df_cat %>% 
+  select(total_sp, ID)
+
+occurrence_proportion_df_cat_tidy <- occurrence_proportion_df_cat %>% 
+  gather(diversity_type, diversity_measure, -c(ID, Year, num_native_sp, num_nonnative_sp,
+                                               prop_nonnative_sp, Year1, Year2, category)) %>% 
+  mutate(diversity_type = case_when(diversity_type == "shannon_div" ~ "Shannon diversity index",
+                                   diversity_type == "simpson_div" ~ "Simpson diversity index",
+                                   diversity_type == "total_sp" ~ "Species richness"))
+
+occurrence_prop_sp_fig <- ggplot(occurrence_proportion_df_cat_tidy, aes(category, diversity_measure, fill = category))+
+  geom_boxplot(position =position_dodge())+
+  geom_point(position = position_jitterdodge(0.25), alpha = .75)+
+  stat_compare_means(comparisons = my_comparisons, aes(label = ..p.signif..))+
+  scale_fill_manual(values = wes_palette("GrandBudapest1"))+
+  scale_color_manual(values = wes_palette("GrandBudapest1"))+
+  labs(y = "Alpha diversity",
+       x = "Proportion of non-native species (by occurrence with P.flavens as native)")+
+  theme_classic()+
+  theme(text = element_text(family = "Times New Roman", size = 14),
+        legend.position = "bottom",
+        legend.title = element_blank())+
+  facet_grid(diversity_type~., scales="free_y")
 
 
 
@@ -434,6 +505,42 @@ non_native_propotion_df_NN <- total_abu_NN %>%
   bind_cols(shannon_div_df,
             simpson_div_df)
 
+
+non_native_propotion_df_NN_cat <- non_native_propotion_df_NN %>% 
+  filter(total_abundance != 0) %>% 
+  mutate(category = ifelse(non_native_abu_prop < .33, "Low", 
+                           ifelse(non_native_abu_prop > .66, "High", "Medium"))) %>% 
+  left_join(sp_richness, by = "ID")
+
+non_native_propotion_df_NN_cat$category <- factor(non_native_propotion_df_NN_cat$category, levels = c("Low", "Medium", "High"))
+
+my_comparisons <- list(   c("High", "Medium"),  c("Low", "Medium"), c("Low", "High") )
+
+
+
+non_native_propotion_df_NN_cat_tidy <- non_native_propotion_df_NN_cat %>% 
+  select(ID, Year, non_native_abu_prop, shannon_div, simpson_div, total_sp, category) %>% 
+  gather(diversity_type, diversity_measure, -c(ID, Year, non_native_abu_prop, category)) %>% 
+  mutate(diversity_type = case_when(diversity_type == "shannon_div" ~ "Shannon diversity index",
+                                    diversity_type == "simpson_div" ~ "Simpson diversity index",
+                                    diversity_type == "total_sp" ~ "Species richness"))
+
+abundance_prop_alpha_fig_NN <- ggplot(non_native_propotion_df_NN_cat_tidy, aes(category, diversity_measure, fill = category))+
+  geom_boxplot(position =position_dodge())+
+  geom_point(position = position_jitterdodge(0.25), alpha = .75)+
+  stat_compare_means(comparisons = my_comparisons, aes(label = ..p.signif..))+
+  scale_fill_manual(values = wes_palette("GrandBudapest1"))+
+  scale_color_manual(values = wes_palette("GrandBudapest1"))+
+  labs(y = "Alpha diversity",
+       x = "Proportion of non-native species (by abundance with P.flavens as non-native)")+
+  theme_classic()+
+  theme(text = element_text(family = "Times New Roman", size = 14),
+        legend.position = "bottom",
+        legend.title = element_blank())+
+  facet_grid(diversity_type~., scales="free_y")
+
+
+plot_grid(occurrence_prop_sp_fig, abundance_prop_alpha_fig)
 #  linear model
 lm_fit <- lm(shannon_div ~ non_native_abu_prop, data=non_native_propotion_df_NN)
 summary(lm_fit)
@@ -474,7 +581,7 @@ ggplot(non_native_propotion_df, aes(non_native_abu_prop, simpson_div))+
        y = "Shannon diversity index")
 
 ### do the same as above but this time by species occurrence (presence/absence) rather than abundance (Pheidole flavens complex as native)
-data_spacc_all_tidy_occ <- data_spacc_all_tidy_PF_non_native %>% 
+data_spacc_all_tidy_occ_NN <- data_spacc_all_tidy_PF_non_native %>% 
   filter(abundance != 0) %>%  # sp has to be present to contribute
   filter(Native == 1) %>% 
   group_by(ID, Year) %>% 
@@ -482,11 +589,11 @@ data_spacc_all_tidy_occ <- data_spacc_all_tidy_PF_non_native %>%
 
 data_spacc_all_tidy_non_native <- data_spacc_all_tidy %>% 
   filter(abundance != 0) %>%  # sp has to be present to contribute
-  filter(Native == 0 | Native == 2) %>% 
+  filter(Native == 0) %>% 
   group_by(ID, Year) %>% 
   summarize(num_nonnative_sp = n())
 
-occurrence_proportion_df <- data_spacc_all_tidy_occ %>% 
+occurrence_proportion_df_NN <- data_spacc_all_tidy_occ_NN %>% 
   full_join(data_spacc_all_tidy_non_native) %>% 
   mutate_all(~replace(., is.na(.), 0)) %>% 
   mutate(total_sp = num_native_sp + num_nonnative_sp,
@@ -496,6 +603,42 @@ occurrence_proportion_df <- data_spacc_all_tidy_occ %>%
   arrange(ID) %>% 
   bind_cols(shannon_div_df, 
             simpson_div_df)
+
+occurrence_proportion_df_cat_NN <- occurrence_proportion_df_NN %>% 
+  filter(total_sp != 0) %>% 
+  mutate(category = ifelse(prop_nonnative_sp < .33, "Low", 
+                           ifelse(prop_nonnative_sp > .66, "High", "Medium")))
+
+occurrence_proportion_df_cat_NN$category <- factor(occurrence_proportion_df_cat_NN$category, levels = c("Low", "Medium", "High"))
+
+my_comparisons <- list(   c("High", "Medium"),  c("Low", "Medium"), c("Low", "High") )
+
+
+occurrence_proportion_df_cat_tidy_NN <- occurrence_proportion_df_cat_NN %>% 
+  gather(diversity_type, diversity_measure, -c(ID, Year, num_native_sp, num_nonnative_sp,
+                                               prop_nonnative_sp, Year1, Year2, category)) %>% 
+  mutate(diversity_type = case_when(diversity_type == "shannon_div" ~ "Shannon diversity index",
+                                    diversity_type == "simpson_div" ~ "Simpson diversity index",
+                                    diversity_type == "total_sp" ~ "Species richness"))
+
+occurrence_prop_sp_fig_NN <- ggplot(occurrence_proportion_df_cat_tidy_NN, aes(category, diversity_measure, fill = category))+
+  geom_boxplot(position =position_dodge())+
+  geom_point(position = position_jitterdodge(0.25), alpha = .75)+
+  stat_compare_means(comparisons = my_comparisons, aes(label = ..p.signif..))+
+  scale_fill_manual(values = wes_palette("GrandBudapest1"))+
+  scale_color_manual(values = wes_palette("GrandBudapest1"))+
+  labs(y = "Alpha diversity",
+       x = "Proportion of non-native species (by occurrence with P.flavens as non-native)")+
+  theme_classic()+
+  theme(text = element_text(family = "Times New Roman", size = 14),
+        legend.position = "bottom",
+        legend.title = element_blank())+
+  facet_grid(diversity_type~., scales="free_y")
+
+
+plot_grid(occurrence_prop_sp_fig, occurrence_prop_sp_fig_NN, abundance_prop_alpha_fig, abundance_prop_alpha_fig_NN, cols = 2, rows =2)
+#### ^^ Not much (if any) real difference between outcomes of treating P. flavens as native or non-native
+### or by having the proportion of non-native species be based on occurrence or abundance
 
 
 ### for shannon div
@@ -528,7 +671,7 @@ tidy(lm_fit_simp_occ)
 glance(lm_fit_simp_occ)
 
 
-## residuals plot looks more "random" -- linear model is more appropriate for these data than the abundance proportion
+## residuals plot looks more "random" -- linear model is still not v. appropriate for these data than the abundance proportion
 ggplot(occurrence_proportion_df, aes(prop_nonnative_sp, simpson_div))+
   geom_point()+
   geom_line(data = broom::augment(lm_fit_simp_occ), aes(x = prop_nonnative_sp, y = .fitted))+
@@ -540,5 +683,245 @@ ggplot(occurrence_proportion_df, aes(prop_nonnative_sp, simpson_div))+
 
 ## Next step is to pull just Gabriela's data and look at year to year trends + seasonality in species composition
 ## using ordination (nMDS or PCoA and PERMANOVA + permdist)
+data_NMDS <- data_GZ_wide %>% 
+  select(-Year)
 
+
+
+
+# Then calculate multivariate distances between samples using vegdist() with one of various distance metrics:
+dist_jac <- vegdist(data_NMDS, method = "jaccard", binary = TRUE) # Binary jaccard distance (presence/absence)
+dist_BC <- vegdist(data_NMDS, method = "bray")#, binary = TRUE) # Bray-Curtis distance
+
+# Then generate an MDS object using metaMDS():
+mds <- metaMDS(dist_BC)
+str(mds)
+# Plot the mds using basic graphics
+plot(mds)
+
+# Make this into a ggplot:
+# Extract the coordinates:
+points <- as.data.frame(mds$points)
+points
+
+# Add metadata to the points:
+data_GZ_wide <- data_GZ_wide %>% 
+  rownames_to_column()
+
+points <- points %>% mutate(Sample = rownames(points),
+                            Year = data_GZ_wide$Year[match(rownames(points), data_GZ_wide$rowname)]) 
+                            Altitude = metadata$Altitude[match(rownames(points), metadata$Plotid)])
+points
+
+# Q4: Now that we have the MDS ordination data, how can we make it into a nice-looking ggplot?
+
+nmds <- ggplot(data = points, aes(x = MDS1, y = MDS2))+
+  geom_point(aes(color = Year), size = 9)
+
+
+nmds +
+  theme_classic()
+
+## two outliers are really pulling this apart -- drop these - but stress is basically zero -- re-run NMDS without these samples
+outliers_rmvd <- points %>% 
+  filter(MDS1 < .5,
+         MDS2 < .002)
+ggplot(data = outliers_rmvd, aes(x = MDS1, y = MDS2))+
+  geom_point(aes(color = as.factor(Year)))+
+  theme_classic()
+
+
+## pull out these two points (#79 and 168) and drop these from the original df
+outliers_ID <- points %>% 
+  rownames_to_column() %>% 
+  filter(MDS1 > .4 |
+         MDS2 > .002)
+
+data_GZ_wide_new <- data_GZ_wide %>% 
+  filter(rowname != 79,
+         rowname != 168)
+
+
+data_NMDS <- data_GZ_wide_new %>% 
+  select(-Year, -rowname)
+
+
+
+# Then calculate multivariate distances between samples using vegdist() with one of various distance metrics:
+dist_jac <- vegdist(data_NMDS, method = "jaccard", binary = TRUE) # Binary jaccard distance (presence/absence)
+dist_BC <- vegdist(data_NMDS, method = "bray")#, binary = TRUE) # Bray-Curtis distance
+
+# Then generate an MDS object using metaMDS():
+mds <- metaMDS(dist_BC, trymax = 100)
+
+mds2<- metaMDS(dist_BC, trymax = 100, previous.best = mds, noshare=0.1)
+str(mds)
+# Plot the mds using basic graphics
+plot(mds)
+
+# Make this into a ggplot:
+# Extract the coordinates:
+points <- as.data.frame(mds2$points)
+points
+
+# Add metadata to the points:
+data_GZ_wide_new <- data_GZ_wide_new %>% 
+  rownames_to_column()
+
+points <- points %>% mutate(Sample = rownames(points),
+                            Year = data_GZ_wide$Year[match(rownames(points), data_GZ_wide$rowname)]) 
+Altitude = metadata$Altitude[match(rownames(points), metadata$Plotid)])
+points
+
+# Q4: Now that we have the MDS ordination data, how can we make it into a nice-looking ggplot?
+
+nmds <- ggplot(data = points, aes(x = MDS1, y = MDS2))+
+  geom_point(aes(color = Year))
+
+
+nmds +
+  theme_classic()
+
+
+## NMDS isn't converging with all of the individual pitfalls -- likely because many of them have few but differenct ant spp
+## try this again but now grouped by site within year and season
+GZ_wide <- data_GZ_clean %>% 
+  filter(Year == 2014 |
+           Year == 2015,
+         Abundance != "NA") %>% 
+  select(Year, Site, Station, Month, Genus, Species, Abundance) %>% 
+  unite("species", c("Genus", "Species"), remove = T, sep = " ") %>% 
+  # group_by(species) %>% 
+  #  mutate(grouped_id = row_number()) %>% 
+  spread(key = "species", value = "Abundance") %>% 
+  mutate_all(~replace(., is.na(.), 0)) %>%  ## make all the NAs under the species names zeros (for this analysis) 
+group_by(Year, Site, Month) %>% 
+  summarize_at(vars(`Aphaenogaster carolinensis`:`Trachymyrmex septentrionalis`), sum)
+
+GZ_wide_NMDS <- GZ_wide %>% 
+  ungroup() %>% 
+  select(-Year, -Site, -Month)
+
+
+
+# Then calculate multivariate distances between samples using vegdist() with one of various distance metrics:
+dist_jac <- vegdist(GZ_wide_NMDS, method = "jaccard", binary = TRUE) # Binary jaccard distance (presence/absence)
+dist_BC <- vegdist(GZ_wide_NMDS, method = "bray")#, binary = TRUE) # Bray-Curtis distance
+
+# Then generate an MDS object using metaMDS(): ### STILL NOT WORKING WITH NMDS
+mds <- metaMDS(dist_jac, trymax = 100, noshare=0.1)
+
+mds2<- metaMDS(dist_BC, trymax = 100, previous.best = mds, noshare=0.1)
+str(mds)
+# Plot the mds using basic graphics
+plot(mds)
+
+# Make this into a ggplot:
+# Extract the coordinates:
+points <- as.data.frame(mds2$points)
+points
+
+# Add metadata to the points:
+data_GZ_wide_new <- data_GZ_wide_new %>% 
+  rownames_to_column()
+
+points <- points %>% mutate(Sample = rownames(points),
+                            Year = data_GZ_wide$Year[match(rownames(points), data_GZ_wide$rowname)]) 
+Altitude = metadata$Altitude[match(rownames(points), metadata$Plotid)])
+points
+
+# Q4: Now that we have the MDS ordination data, how can we make it into a nice-looking ggplot?
+
+nmds <- ggplot(data = points, aes(x = MDS1, y = MDS2))+
+  geom_point(aes(color = Year))
+
+
+nmds +
+  theme_classic()
+
+
+## trying PoA
+library(ape)
+PCOA <- pcoa(dist_BC)
+
+
+# plot the eigenvalues and interpret
+barplot(PCOA$values$Relative_eig[1:10])
+# Can you also calculate the cumulative explained variance of the first 3 axes?
+
+# Some distance measures may result in negative eigenvalues. In that case, add a correction:
+PCOA <- pcoa(dist, correction = "cailliez")
+
+# Plot your results
+biplot.pcoa(PCOA)
+
+# You see what`s missing? 
+# Indeed, there are no species plotted on this biplot. 
+# That's because we used a dissimilarity matrix (sites x sites) 
+# as input for the PCOA function. 
+# Hence, no species scores could be calculated. 
+#However, we could work around this problem like this:
+biplot.pcoa(PCOA, GZ_wide_NMDS)
+
+
+
+# Make this into a ggplot:
+# Extract the coordinates:
+points <- as.data.frame(PCOA$vectors) %>% 
+  select(Axis.1, Axis.2) %>% 
+  rownames_to_column()
+points
+
+# Add metadata to the points:
+GZ_wide_2 <- GZ_wide %>% 
+  rownames_to_column()
+
+points <- points %>% mutate(Sample = rownames(points),
+                            Year = GZ_wide_2$Year[match(rownames(points), GZ_wide_2$rowname)],
+                            Month = GZ_wide_2$Month[match(rownames(points), GZ_wide_2$rowname)]) 
+
+points
+
+# Q4: Now that we have the MDS ordination data, how can we make it into a nice-looking ggplot?
+
+points<- points %>% 
+  unite(month_yr, c("Month", "Year"), remove = F)
+
+PCoA<- ggplot(data = points, aes(x = Axis.1, y = Axis.2, color = as.factor(Month)))+
+  geom_point(aes(shape = as.factor(Year)))+
+  stat_ellipse()+
+  theme_classic()
+
+
+PCoA
+
+adonis(dist_BC ~ Year*Month, data = GZ_wide_2, strata = GZ_wide_2$Site)
+## Composition in ant community affected by seasonality but not by year
+
+## check for homogeneity in dispersion - if there is, it may invalidate the PERMANOVA sp. composition results
+
+group <- GZ_wide_2$Month
+## Calculate multivariate dispersions
+mod <- betadisper(dist_BC, group)
+mod
+
+## Perform test
+anova(mod)
+
+## Permutation test for F
+permutest(mod, pairwise = TRUE)
+
+## Tukey's Honest Significant Differences
+(mod.HSD <- TukeyHSD(mod))
+plot(mod.HSD)
+
+## Plot the groups and distances to centroids on the
+## first two PCoA axes
+plot(mod)
+
+## Draw a boxplot of the distances to centroid for each group
+boxplot(mod)
+
+## no difference in homogeneity of dispersion, can continue with the seasonality differences being due to compositional differences
+## rather than being driven by differences in composition within sample groups (higher vs lower beta diversity)
 
